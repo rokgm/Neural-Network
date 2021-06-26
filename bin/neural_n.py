845318f44b8e7351ee898dc92b07249b69bcac5b
Rok Grgič Meško <rok.grgicmesko@gmail.com>
2021-06-26 23:47:00 +0200

    narejen feedforward, dodan test v git ignore

--- bin/neural_n.py
+++ bin/neural_n.py
@@ -1,52 +1,61 @@
 import numpy as np
 
+from sigmoid import sigmoid, deriv_sigmoid
+
 
 class NeuralNetwork():
 
-    def __init__(self, input_n, hidden_n, output_n):
-        """
-        Create neural network structure.
+    def __init__(self, input_size, hidden_size, output_size):
+        """Create neural network structure.
 
         Args:
-            input_n (int): number of input neurons
-            hidden_n (int): number of hidden neurons
-            output_n (int): number of output neurons
+            input_size (int): number of input neurons
+            hidden_size (int): number of hidden neurons
+            output_size (int): number of output neurons
         """     
-        self.input_n = input_n
-        self.hidden_n = hidden_n
-        self.output_n = output_n
-        self.weights1 = np.random.rand(self.hidden_n, self.input_n) * np.sqrt(1. / self.input_n)
-        self.weights2 = np.random.randn(self.output_n, self.hidden_n) * np.sqrt(1. / self.hidden_n)
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.weights1 = np.random.rand(self.hidden_size, self.input_size) * np.sqrt(1. / self.input_size)
+        self.weights2 = np.random.randn(self.output_size, self.hidden_size) * np.sqrt(1. / self.hidden_size)
 
     def __repr__(self):
-        return 'NeuralNetwork(input_n={0.input_n}, hidden_n={0.hidden_n}, output_n={0.output_n})'.format(self)
+        return 'NeuralNetwork(input_size={0.input_size}, hidden_size={0.hidden_size}, output_size={0.output_size})'.format(self)
 
     def add_input(self, lst):
-        """
-        Add input data to neural network.
+        """Add input data to neural network.
 
         Args:
             lst (list)
         """
         lst = np.array(lst) 
-        if self.input_n == len(lst):
+        if self.input_size == len(lst):
             self.input = lst
         else:
             raise ValueError('Size of input and number of input neurons do not match.')
 
-    def add_output(self, lst):
-        """
-        Add output data to neural network.
+    def add_output(self, lst):          # popravi, ali rabim cel lst ali samo float?
+        """Add output data to neural network.
 
         Args:
             lst (list)
         """
         lst = np.array(lst) 
-        if self.output_n == len(lst):
+        if self.output_size == len(lst):
             self.output = lst
         else:
-            raise ValueError('Size of output and number of input neurons do not match.')
+            raise ValueError('Size of output and number of output neurons do not match.')
+
+    def feed_forward(self):       
+        """Propagates input through the network.
+
+        Returns:
+            np.array: output of network
+        """        
+        return sigmoid(np.matmul(self.weights2, sigmoid(np.matmul(self.weights1, self.input))))
 
 net = NeuralNetwork(4,3,2)
 net.add_input([2,2,2,2])
 net.add_output([2,2])
+
+print(NeuralNetwork.feed_forward(net))
